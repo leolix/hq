36453a53f95f0f2344bc52a4cf85d953e2f0fadc
leolix <leolix@mail.ru>
2014-06-17 17:09:45 +0400

    Create test_exam_spec.rb

---
+++ spec/models/test/test_exam_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Exam do
+  describe 'обладает связями с другими моделями:' do
+    it 'с предметом' do
+      expect belong_to(:subject)
+    end
+
+    it 'с группой' do
+      expect belong_to(:group)
+    end
+
+    it 'со студентом' do
+      expect belong_to(:student)
+    end
+
+    it 'c экзаменом студента' do
+      expect have_many(:exam_student)
+    end
+
+    it 'c оценками' do
+      expect have_many(:marks)
+    end
+  end
+end
