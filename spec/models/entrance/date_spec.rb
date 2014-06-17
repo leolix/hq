16204c2f28c025f7a421b034e818bdc3cb5b3f62
leolix <leolix@mail.ru>
2014-06-17 19:14:57 +0400

    Create date_spec.rb

---
+++ spec/models/entrance/date_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Entrance do
+  describe 'обладает связями с другими моделями:' do
+    it 'c приемной кампанией' do
+      expect have_many(:campaign)
+    end
+
+    it 'с формой образования' do
+      expect belong_to(:education_form)
+    end
+
+    it 'с типом образования' do
+      expect belong_to(:education_type)
+    end
+
+    it 'с пройденым образованием' do
+      expect belong_to(:education_source)
+    end
+  end
+end
